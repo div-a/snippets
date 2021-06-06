629f57ad343151e351c4c56a0c974c38adf4a772
Diva Lonial <diva.lonial@gmail.com>
2021-06-06 16:07:34 +0800

    Add electron integration. Be able to use electron library within react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+const { clipboard } = window.require("electron")
 
 function App() {
+
+  useEffect(() => {
+    console.log(clipboard.readText())
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. cHI
+
+          {clipboard.readText()}
         </p>
         <a
           className="App-link"
