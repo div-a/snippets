7e52dc11db84ec5e7e50c926ad0f2afdda87b55a
Diva Lonial <diva.lonial@gmail.com>
2021-06-06 20:13:08 +0800

    Add global shortcut event, get electron to access clipboard

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, globalShortcut, BrowserWindow, ipcMain } = require('electron');
 const isDev = require('electron-is-dev');
 const path = require('path');
 
@@ -11,13 +11,18 @@ function createWindow() {
         show: false,
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false
+            contextIsolation: false,
+            preload: __dirname + '/preload.js'
         }
     });
+
     const startURL = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`;
 
     mainWindow.loadURL(startURL);
 
+    globalShortcut.register('CommandOrControl+G', () => {
+        console.log('Electron loves global shortcuts!')
+    })
     mainWindow.once('ready-to-show', () => mainWindow.show());
     mainWindow.on('closed', () => {
         mainWindow = null;
--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-const { clipboard } = window.require("electron")
+const { clipboard, ipcRenderer } = window.require("electron")
 
 function App() {
 
+  const [clipboardText, setClipboard] = useState('')
+
+  const doStuff = () => {
+    console.log("doing stuff")
+    setClipboard(clipboard.readText());
+    setTimeout(doStuff, 1000);
+  }
+
   useEffect(() => {
-    console.log(clipboard.readText())
-  }, [])
+    doStuff();
+  })
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. cHI
+          Edit <code>src/App.js</code> and save to reload.
 
-          {clipboard.readText()}
+          {clipboardText}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
