abf6c6bf1293414c38bad0f587e64376c2618560
Diva Lonial <diva.lonial@gmail.com>
2021-06-13 14:39:52 +0800

    Rename variables / functions. Make use effect not async. Move out styled divs. Bug with setting snippets on page click.

--- src/App.js
+++ src/App.js
@@ -15,104 +15,111 @@ db.version(1).stores(
   }
 )
 
+
+const Input = styled.input`
+color: palevioletred;
+font-size: 1em;
+border: 2px solid palevioletred;
+border-radius: 3px;
+margin: 0.5em;
+padding: 0.5em;
+background-color: transparent;
+`;
+
+const SaveButton = styled.button`
+color: palevioletred;
+font-size: 1em;
+border: 2px solid palevioletred;
+border-radius: 3px;
+margin: 0.5em;
+padding: 0.5em;
+background-color: transparent;
+`;
+
+const SnippetList = styled.div`
+backgroud-color: #282c34;
+height: 80vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: flex-start;
+overflow-y: scroll;
+`;
+
+const PageList = styled.div`
+backgroud-color: #282c34;
+height: 80vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: flex-start;
+overflow-y: scroll;
+min-width: 20%;
+`;
+
+const PageButton = styled.button`
+background: transparent;
+border-radius: 3px;
+border: 2px solid palevioletred;
+color: palevioletred;
+margin: 0 1em;
+padding: 0.25em 1em;
+margin: 0.25em;
+`;
+
 function App() {
 
-  const Input = styled.input`
-    color: palevioletred;
-    font-size: 1em;
-    border: 2px solid palevioletred;
-    border-radius: 3px;
-    margin: 0.5em;
-    padding: 0.5em;
-    background-color: transparent;
-  `;
-
-  const SaveButton = styled.button`
-    color: palevioletred;
-    font-size: 1em;
-    border: 2px solid palevioletred;
-    border-radius: 3px;
-    margin: 0.5em;
-    padding: 0.5em;
-    background-color: transparent;
-  `;
-
-  const SnippetList = styled.div`
-    backgroud-color: #282c34;
-    height: 80vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-start;
-    overflow-y: scroll;
-
-  `;
-
-  const PageList = styled.div`
-    backgroud-color: #282c34;
-    height: 80vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-start;
-    overflow-y: scroll;
-  `;
-
-  const PageButton = styled.button`
-    background: transparent;
-    border-radius: 3px;
-    border: 2px solid palevioletred;
-    color: palevioletred;
-    margin: 0 1em;
-    padding: 0.25em 1em;
-    margin: 0.25em;
-  `;
 
   const [allSnippets, setAllSnippets] = useState([])
-  const [page, setPage] = useState({})
+  const [page, setPage] = useState({ name: "" })
   const [allPages, setAllPages] = useState([])
 
 
-  const doStuff = () => {
-    const text = clipboard.readText();
-    if (allSnippets.length == 0 || (allSnippets.length > 0 && text != allSnippets[allSnippets.length - 1])) {
-      setAllSnippets([...allSnippets, text])
-      db.Snippet.add({
-        text,
+  const addSnippet = async () => {
+    const newSnippet = clipboard.readText();
+    if (allSnippets.length == 0 || (allSnippets.length > 0 && newSnippet != allSnippets[allSnippets.length - 1])) {
+      setAllSnippets([...allSnippets, newSnippet])
+      await db.Snippet.add({
+        text: newSnippet,
         pageId: page.id
       })
     }
   };
 
   useEffect(() => {
-    ipcRenderer.on('asynchronous-message', () => { doStuff(); });
+    ipcRenderer.on('asynchronous-message', async () => { await addSnippet(); });
 
     return () => {
       ipcRenderer.removeAllListeners();
     };
   }, [allSnippets]);
 
-  useEffect(async () => {
-    const pageSnippets = await db.Snippet.where("pageId").equalsIgnoreCase(page.id?.toString() || "").toArray();
-    if (pageSnippets && pageSnippets.length > 0) {
-      setAllSnippets(pageSnippets)
+  useEffect(() => {
+    async function fetchData() {
+      if (page.id) {
+        const pageSnippets = await db.Snippet.where("pageId").anyOf([page.id]).toArray();
+        setAllSnippets(pageSnippets.map(ps => ps.text))
+      }
     }
+
+
   }, [page]);
 
 
-  useEffect(async () => {
-    const newPageName = new Date().toISOString().substring(0, 19);
-    await db.Page.add({
-      name: newPageName
-    });
+  useEffect(() => {
+    async function fetchData() {
+      const newPageName = new Date().toISOString().substring(0, 19);
+      const res = await db.Page.add({
+        name: newPageName
+      });
+      setPage(await db.Page.where({ id: res }).first());
+      setAllPages(await db.Page.toArray())
+    }
 
-    setPage(await db.Page.where("name").equalsIgnoreCase(newPageName).first());
-
-    setAllPages(await db.Page.toArray())
+    fetchData();
   }, [])
 
   const selectPage = async (event) => {
-    console.log(event.target.innerText);
     const selectedPage = allPages.filter(ap => ap.name === event.target.innerText)[0];
     setPage(selectedPage)
   }
