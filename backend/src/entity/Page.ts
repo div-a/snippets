d6f0c920d5d3aaf1cd29194f7911e672892e73dd
DESKTOP-3G40DR1\Diva <diva.lonial@gmail.com>
2021-08-30 21:03:58 +0800

    Additional mutations for updating, creating

--- backend/src/entity/Page.ts
+++ backend/src/entity/Page.ts
@@ -1,10 +1,14 @@
+import { Field, Int, ObjectType } from "type-graphql";
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 
+@ObjectType()
 @Entity()
 export class Page extends BaseEntity {
-  @PrimaryGeneratedColumn()
+  @Field(() => Int) // for type-graphql
+  @PrimaryGeneratedColumn() // for orm
   id: number;
 
+  @Field()
   @Column()
   name: string;
 }
--- backend/src/resolvers/PageResolver.ts
+++ backend/src/resolvers/PageResolver.ts
@@ -1,13 +1,36 @@
 import { Page } from "../entity/Page";
-import { Arg, Mutation, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PageResolver {
+  @Mutation(() => Page)
+  async createPage(@Arg("name") name: string) {
+    const page = await Page.create({
+      name,
+    }).save();
+    return page;
+  }
+
   @Mutation(() => Boolean)
-  async CreatePage(@Arg("name") name: string) {
-    const page = new Page();
-    page.name = name;
-    await page.save();
+  async updatePage(@Arg("id") id: number, @Arg("newName") newName: string) {
+    await Page.update(
+      { id },
+      {
+        name: newName,
+      }
+    );
     return true;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePage(@Arg("id") id: number) {
+    await Page.delete({ id });
+    return true;
+  }
+
+  @Query(() => [Page])
+  async pages() {
+    let res = await Page.find();
+    return res;
   }
 }
